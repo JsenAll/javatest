0b27c3c74f7df522fd7f4b60c24887fdd4b791fd
jhs <724033319@qq.com>
2019-08-08 11:44:59 +0800

    单项的多次发送 多次接受

--- study/src/main/java/com/java/study/internet/study03_udp/study03_02/UdpTalkClient.java
+++ study/src/main/java/com/java/study/internet/study03_udp/study03_02/UdpTalkClient.java
@@ -1,10 +1,13 @@
 package com.java.study.internet.study03_udp.study03_02;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 
-/**UDP 编程  客户端
+/**
+ * UDP 编程  客户端
  * 1:使用DatagramSocket 指定端口 创建发送端
  * 2:准备数据 一定转成字节数组、
  * 3:封装成DatagramPacket 包裹，需要指定目的地
@@ -16,14 +19,22 @@ public class UdpTalkClient {
         System.out.println("发送启动中");
         //1:使用DatagramSocket 指定端口 创建发送端
         DatagramSocket client = new DatagramSocket(8888);
-        //2:准备数据 一定转成字节数组
-        String data = "I LOVE YOU";
-        byte[] datas = data.getBytes();
-        //3：封装成DatagramPacket 包裹，需要指定目的地
-        DatagramPacket packet = new DatagramPacket(datas, 0, datas.length,
-                new InetSocketAddress("localhost", 9999));
-        //4 发送包裹
-        client.send(packet);
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            //2:准备数据 一定转成字节数组
+            String data = reader.readLine();
+            byte[] datas = data.getBytes();
+            //3：封装成DatagramPacket 包裹，需要指定目的地
+            DatagramPacket packet = new DatagramPacket(datas, 0, datas.length,
+                    new InetSocketAddress("localhost", 9999));
+            //4 发送包裹
+            client.send(packet);
+            if (data.equals("baibai")) {
+                break;
+            }
+
+        }
         //5:释放资源
         client.close();
     }
--- study/src/main/java/com/java/study/internet/study03_udp/study03_02/UdpTalkServer.java
+++ study/src/main/java/com/java/study/internet/study03_udp/study03_02/UdpTalkServer.java
@@ -16,15 +16,22 @@ public class UdpTalkServer {
         System.out.println("接受方启动中....");
 //         * 1:使用DatagramSocket 指定端口 创建接收端
         DatagramSocket datagramSocket = new DatagramSocket(9999);
+        while (true) {
 //         * 2:准备容器 封装成DatagramPacket包裹
-        byte[] container = new byte[1024 * 60];
-        DatagramPacket packet = new DatagramPacket(container, 0, container.length);
+            byte[] container = new byte[1024 * 60];
+            DatagramPacket packet = new DatagramPacket(container, 0, container.length);
 //         * 3:阻塞式接收包裹 receive(DatagramPacket p)
-        datagramSocket.receive(packet);
+            datagramSocket.receive(packet);
 //         * 4:分析数据
-        byte[] datas = packet.getData();
-        System.out.println(new String(datas, 0, datas.length));
-        System.out.println(packet.getLength());
+            byte[] datas = packet.getData();
+            int len = packet.getLength();
+
+            String data = new String(datas, 0, len);
+            System.out.println(data);
+            if (data.equals("baibai")) {
+                break;
+            }
+        }
 //         * 5:释放资源
         datagramSocket.close();
 
