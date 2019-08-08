c120d51de5958e1122835f5bef8fc1a6b3b5eb61
jhs <724033319@qq.com>
2019-08-08 10:42:47 +0800

    完善 UDP通讯

--- study/src/main/java/com/java/study/internet/study03_udp/UdpServer.java
+++ study/src/main/java/com/java/study/internet/study03_udp/UdpServer.java
@@ -1,4 +1,33 @@
 package com.java.study.internet.study03_udp;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+/**
+ * UDP 编程  接收端
+ * 1:使用DatagramSocket 指定端口 创建接收端
+ * 2:准备容器 封装成DatagramPacket包裹
+ * 3:阻塞式接收包裹 receive(DatagramPacket p)
+ * 4:分析数据
+ * 5:释放资源
+ */
 public class UdpServer {
+    public static void main(String[] args) throws Exception {
+        System.out.println("接受方启动中....");
+// * 1:使用DatagramSocket 指定端口 创建接收端
+        DatagramSocket datagramSocket = new DatagramSocket(9999);
+
+        while (true) {
+            byte[] container = new byte[1024 * 60];
+            DatagramPacket packet = new DatagramPacket(container, 0, container.length);
+            datagramSocket.receive(packet);
+            byte[] datas = packet.getData();
+            System.out.println(new String(datas, 0, datas.length));
+            System.out.println(packet.getLength());
+        }
+//        datagramSocket.close();
+
+
+    }
 }
