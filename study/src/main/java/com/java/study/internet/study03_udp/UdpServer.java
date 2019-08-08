db8060eb34e756c4739558e378b074a4c553e6a4
jhs <724033319@qq.com>
2019-08-08 11:10:06 +0800

    基础 UDP通讯

--- study/src/main/java/com/java/study/internet/study03_udp/UdpServer.java
+++ study/src/main/java/com/java/study/internet/study03_udp/UdpServer.java
@@ -15,18 +15,19 @@ import java.net.SocketException;
 public class UdpServer {
     public static void main(String[] args) throws Exception {
         System.out.println("接受方启动中....");
-// * 1:使用DatagramSocket 指定端口 创建接收端
+//         * 1:使用DatagramSocket 指定端口 创建接收端
         DatagramSocket datagramSocket = new DatagramSocket(9999);
-
-        while (true) {
-            byte[] container = new byte[1024 * 60];
-            DatagramPacket packet = new DatagramPacket(container, 0, container.length);
-            datagramSocket.receive(packet);
-            byte[] datas = packet.getData();
-            System.out.println(new String(datas, 0, datas.length));
-            System.out.println(packet.getLength());
-        }
-//        datagramSocket.close();
+//         * 2:准备容器 封装成DatagramPacket包裹
+        byte[] container = new byte[1024 * 60];
+        DatagramPacket packet = new DatagramPacket(container, 0, container.length);
+//         * 3:阻塞式接收包裹 receive(DatagramPacket p)
+        datagramSocket.receive(packet);
+//         * 4:分析数据
+        byte[] datas = packet.getData();
+        System.out.println(new String(datas, 0, datas.length));
+        System.out.println(packet.getLength());
+//         * 5:释放资源
+        datagramSocket.close();
 
 
     }
